43f34661fd8cc8101e99c988dc9116dd3e350ed5
Steve De Jongh <de.jongh.steve@gmail.com>
2023-12-26 15:42:29 -0800

    Start of LS225

---
+++ lesson_2.js
@@ -0,0 +1,15 @@
+// Lesson 2: Walthrough: Object Methods.
+
+let me = {
+  firstName: 'Steve',
+  lastName: 'De Jongh',
+};
+
+/* Alternatively, we can start with an empty object and add the properties in later.
+
+let me = {};
+me.firstName = 'Steve';
+me.lastNAme = 'De Jongh';
+
+*/
+
