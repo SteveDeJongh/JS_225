f9f1ce892bc9a82f07ddfeb1a06ee687d33dbe53
Steve De Jongh <de.jongh.steve@gmail.com>
2023-12-27 11:56:45 -0800

    JS 225 Lesson 2

---
+++ lesson_2/practice_problems_mutating_objects.js
@@ -0,0 +1,26 @@
+// Lesson 2: Practice Problems Mutating Objects
+
+// 1 
+
+"Hello from the function scope!"
+"Hello from the global scope!"
+
+// 2
+
+"Greetings from the function scope!"
+"Greetings from the function scope!"
+
+// 3
+
+"Hello from the function scope"
+"Hello from the function scope"
+
+// 4
+
+false
+true
+
+// 5
+
+// Because on line 10, we are not mutating the Object, but rather changing the object referenced by animal.
+
