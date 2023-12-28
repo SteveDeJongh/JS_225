c588125a3437843fb4a8ed4bd5246a3e06bdc097
Steve De Jongh <de.jongh.steve@gmail.com>
2023-12-27 16:05:45 -0800

    JS 225 Objects

---
+++ JS_225_Object_Oriented_JavaScript/Objects/problem_5.js
@@ -0,0 +1,54 @@
+// School
+
+// Student object
+
+function createStudent(name, year) {
+  return {
+    name,
+    year,
+    courses: [],
+    info() {
+      console.log(`${this.name} is a ${this.year} year student`);
+    },
+
+    listCourses() {
+      return this.courses;
+    },
+
+    addCourse(course) {
+      this.courses.push(course);
+    },
+
+    addNote(courseCode, note) {
+      const course = this.courses.filter(({code}) => code === courseCode)[0];
+
+      if (course) {
+        if (course.note) {
+          course.note += `; ${note}`;
+        } else {
+          course.note = note;
+        }
+      }
+
+    },
+
+    viewNotes() {
+      this.courses.forEach(course => {
+        if (course.note) {
+          console.log(`${course.name}: ${course.note}`);
+        }
+      });
+    },
+
+    updateNote(courseCode, note) {
+      const course = this.courses.filter(({code}) => code === courseCode)[0];
+
+      if (course) {
+        course.note = note;
+      }
+    },
+  };
+}
+
+// School
+
