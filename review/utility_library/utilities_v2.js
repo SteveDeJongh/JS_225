58f88766fc304fc194921a15e97fdaeb38f23860
Steve De Jongh <de.jongh.steve@gmail.com>
2024-01-11 10:11:57 -0800

    JS 229 Part 2 prep

--- review/utility_library/utilities_v2.js
+++ review/utility_library/utilities_v2.js
@@ -47,7 +47,56 @@
         }
 
         return sampleArr;
-      }
+      },
+
+      findWhere(matchMe) {
+        let mkey = Object.keys(matchMe);
+        let match = undefined;
+
+        for (let i = 0; i < element.length; i += 1) {
+          let curr = element[i];
+          if (mkey.every(k => matchMe[k] === curr[k])) {
+            match = curr;
+            break;
+          }
+        }
+
+        return match;
+      },
+
+      where(matchMe) {
+        let mkey = Object.keys(matchMe);
+        let matches = [];
+
+        for (let i = 0; i < element.length; i += 1) {
+          let curr = element[i];
+          if (mkey.every(k => matchMe[k] === curr[k])) {
+            matches.push(curr);
+          }
+        }
+
+        return matches;
+      },
+
+      pluck(key) {
+        let values = [];
+
+        element.forEach(obj => {
+          if (obj[key] !== undefined) {
+            values.push(obj[key]);
+          }
+        })
+
+        return values;
+      },
+
+      keys() {
+        return Object.keys(element);
+      },
+
+      values() {
+        return Object.values(element);
+      },
     };
 
     return u;
@@ -65,6 +114,19 @@
       }
       return result;
     };
+
+    _.extend = function() {
+      let args = [].slice.call(arguments, 0);
+
+      for (let i = args.length - 1; i > 0; i -= 1) {
+        let curr = args[i];
+
+        Object.keys(curr).forEach(key => {
+          args[i - 1][key] = curr[key];
+        });
+      }
+      return args[0];
+    },
 
   window._ = _;
 })();
